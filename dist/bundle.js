7396d9b8917ebfa5461fd84e18a5ea588a62f217
Uhsmt <owofib@gmail.com>
2024-09-26 21:38:56 +0900

    update

--- dist/bundle.js
+++ dist/bundle.js
@@ -6216,7 +6216,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /******/ 	
 /******/ 	/* webpack/runtime/publicPath */
 /******/ 	(() => {
-/******/ 		__webpack_require__.p = "/";
+/******/ 		__webpack_require__.p = "/LoopGame/";
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/jsonp chunk loading */
