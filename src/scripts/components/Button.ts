9d832c859f6bab158069085ce2236c39042bdc93
Uhsmt <owofib@gmail.com>
2024-10-24 00:01:05 +0900

    Refactor Button component to add leaf sprite and adjust text positioning

--- src/scripts/components/Button.ts
+++ src/scripts/components/Button.ts
@@ -6,26 +6,35 @@ export class Button extends BaseCaptureableObject {
     private buttonText: PIXI.Text;
     constructor(text: string, x: number, y: number) {
         super();
+        const leafSprite = new PIXI.Sprite(PIXI.Texture.from("leaf"));
+        leafSprite.scale.set(0.5);
+        this.addChild(leafSprite);
+
         this.buttonText = new PIXI.Text({
             text: text,
             style: {
                 fontFamily: Const.FONT_ENGLISH,
                 fontWeight: Const.FONT_ENGLISH_BOLD,
-                fontSize: 40,
+                fontSize: 30,
                 fill: "#ffffff",
                 align: "center",
             },
         });
         this.addChild(this.buttonText);
-        this.interactive = true;
+        this.buttonText.anchor.set(0.5);
+        this.buttonText.x = (1.1 * leafSprite.width) / 2;
+        this.buttonText.y = leafSprite.height / 2;
+
         this.x = x;
         this.y = y;
+        this.pivot.set(this.width / 2, this.height / 2);
+        this.hitAreaSize = this.buttonText.height / 2;
     }
 
     protected getObjectCenter(): { x: number; y: number } {
         return {
-            x: this.x + this.width / 2,
-            y: this.y + this.height / 2,
+            x: this.x,
+            y: this.y,
         };
     }
 
