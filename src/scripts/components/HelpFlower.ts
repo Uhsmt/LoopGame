8286888944a5b8f7d3f710a08158e03e8607b26e
Uhsmt <owofib@gmail.com>
2024-10-25 23:50:48 +0900

    Refactor HelpFlower class to add message descriptions and Japanese translations

--- src/scripts/components/HelpFlower.ts
+++ src/scripts/components/HelpFlower.ts
@@ -6,7 +6,7 @@ export class HelpFlower extends BaseCaptureableObject {
     private type: string;
     private elapsedX: number = 0; // 時間パラメータ
     private elapsedY: number = 0; // 時間パラメータ
-    message: string;
+    readonly message: string;
     isRunning: boolean = true;
     private sprite: PIXI.Sprite;
     private readonly screenWidth: number;
@@ -16,6 +16,9 @@ export class HelpFlower extends BaseCaptureableObject {
     private readonly coefficientY: number = Utility.random(50, 70) / 100; // Y軸の振動の係数
     private readonly coefficientX: number =
         (Utility.chooseAtRandom([-1, 1], 1)[0] * Utility.random(50, 70)) / 100; // X軸の振動の係数
+    readonly description: string;
+    readonly descriptionJP: string;
+    readonly messageJP: string;
 
     constructor(type: string, screenWidth: number, screenHeight: number) {
         super();
@@ -24,19 +27,31 @@ export class HelpFlower extends BaseCaptureableObject {
             case "freeze":
                 this.sprite = PIXI.Sprite.from("flower1");
                 this.message = "Freeze!";
+                this.messageJP  ="とまれ！";
+                this.description = "Freeze the butterflies";
+                this.descriptionJP = "ちょうちょをとめる";
                 break;
             case "time_plus":
                 this.sprite = PIXI.Sprite.from("flower2");
                 this.message = "Time extension!";
+                this.messageJP = "もどれ！";
+                this.description = "Extend the game time";
+                this.descriptionJP = "ゲームじかんをのばす";
                 break;
             case "gather":
                 this.sprite = PIXI.Sprite.from("flower3");
                 this.message = "Ghather!";
+                this.messageJP = "あつまれ！";
+                this.description = "Gather the butterflies by color";
+                this.descriptionJP = "ちょうちょをあつめる";
                 break;
             default:
             case "long":
                 this.sprite = PIXI.Sprite.from("flower4");
                 this.message = "Long Loop!";
+                this.messageJP = "ながいループ！";
+                this.description = "Extend the loop line";
+                this.descriptionJP = "ループをのばす";
                 break;
         }
         this.scale.set(0.3);
